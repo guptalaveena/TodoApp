247e1dfaadd42fb6e84bcee32aa036d0932d8774
Laveena Gupta <laveenagupta22111@gmail.com>
2020-04-21 14:25:15 +0530

    Updates

---
+++ precache-manifest.37f4b597f1213aa024f6d451e271ce7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "870a089332dd9e5de48622b00c8b3e0a",
+    "url": "/TodoApp/index.html"
+  },
+  {
+    "revision": "fc57568c8a734770942e",
+    "url": "/TodoApp/static/css/main.92b6c9e0.chunk.css"
+  },
+  {
+    "revision": "7511a072bb7eab288abd",
+    "url": "/TodoApp/static/js/2.da5455b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoApp/static/js/2.da5455b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc57568c8a734770942e",
+    "url": "/TodoApp/static/js/main.b0ede9e1.chunk.js"
+  },
+  {
+    "revision": "be4663f20d7ed2966417",
+    "url": "/TodoApp/static/js/runtime-main.80c41800.js"
+  }
+]);
